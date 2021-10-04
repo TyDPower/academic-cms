f9331cc1dd57021893c872a7510daaae059f6945
Tyrone Dale Power <46249992+TyDPower@users.noreply.github.com>
2021-10-04 12:00:07 +0100

    Update App.js

--- edu-cms/client/src/App.js
+++ edu-cms/client/src/App.js
@@ -1,46 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Students from './libs/components/students/students'
 import './libs/css/App.css';
-import DisplayAllStudents from './libs/components/students/displayAllStudents'
 
 function App() {
-  const [studentID, setStudentID] = useState(null)
-
-  useEffect(() => {
-    console.log(studentID)
-  }, [studentID])
-
-  const display = () => {
-    if (!studentID) {
-      return(
-        <>
-          <DisplayAllStudents 
-            viewStudent={
-              studentID => setStudentID(studentID)
-            }/>
-        </>
-      )
-    }
-
-    return (
-      <>
-        <h1>{studentID}</h1>
-        <button
-          onClick={() => {
-            setStudentID(null)
-          }}
-          type="button" className="btn btn-outline-primary">
-          View Student
-        </button>
-      </>
-    )
-    
-  }
 
   return (
-    <div className="App">
-      {display()}
-    </div>
-  );
+    <>
+      <Students />
+    </>
+  )
+
 }
 
 export default App;
